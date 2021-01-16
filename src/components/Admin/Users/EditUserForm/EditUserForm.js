638f82c5931b69d30d89f553824533c058703814
Camilo Vargas <camilovargasingm@gmail.com>
2021-01-16 03:23:39 -0500

    usando react-dropzone para subir el avatar del usuario

--- src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, {useState, useCallback} from "react";
+import {Avatar, Form, Input, Select, Button, Row, Col} from "antd";
+import {useDropzone} from "react-dropzone";
+import {noAvatar} from "../../../../assets/img";
 
 import "./EditUserForm.scss";
 
 export default function EditUserForm(props) {
     const {user} = props;
+    const [avatar, setAvatar] = useState(null);
+
     return (
-        <div>
-            <h2>Form of edit user</h2>
+        <div className="edit-user-form">
+            <UploadAvatar avatar={avatar} setAvatar={setAvatar} />
             <h2>{user.email}</h2>
+        </div>
+    )
+}
+
+function UploadAvatar(props) {
+    const {avatar, setAvatar} = props;
+    const onDrop = useCallback(
+        acceptedFiles => {
+            const file = acceptedFiles[0];
+            setAvatar({file, preview: URL.createObjectURL(file)})
+        }, 
+        [setAvatar]
+    );
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
+        accept: "image/jpeg, image/png",
+        noKeyboard: true,
+        onDrop
+    });
+
+    return (
+        <div className="upload-avatar" {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? <Avatar size={150} src={noAvatar} /> : 
+            <Avatar size={150} src={avatar ? avatar.preview : noAvatar} />}
         </div>
     )
 }
