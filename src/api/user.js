11f9b24493337f14817effa50ca08a7761eaf82d
Camilo Vargas <camilovargasingm@gmail.com>
2021-01-19 01:23:28 -0500

    Creando funciones Subir Avatar, Obtener Avatar y Actualizar usuario

--- src/api/user.js
+++ src/api/user.js
@@ -80,3 +80,54 @@ export function getUsersActiveApi(token, status){
         return err.message;
     })
 }
+
+export function uploadAvatarApi(token, avatar, userId){
+    const url = `${BASE_PATH}/${API_VERSION}/upload-avatar/${userId}`;
+    const formData = new FormData();
+
+    FormData.append("avatar", avatar, avatar.name);
+    const params = {
+        method: "PUT",
+        body: formData,
+        headers: {
+            Authorization: token
+        }
+    }
+
+    return fetch(url, params).then(response => {
+        return response.json();
+    }).then(result => {
+        return result;
+    }).catch(err => {
+        return err.message;
+    })
+}
+
+export function getAvatarApi(avatarName){
+    const url = `${BASE_PATH}/${API_VERSION}/get-avatar/${avatarName}`;
+    return fetch(url).then(response => {
+        return response.url;
+    }).catch(err => {
+        return err.message;
+    })
+}
+
+export function updateUserApi(token, user, userID){
+    const url = `${BASE_PATH}/${API_VERSION}/update-user/${userID}`;
+    const params = {
+        method: "PUT",
+        body: JSON.stringify(user),
+        headers:  {
+            "Content-Type": "application/json",
+            Authorization: token
+        }
+    }
+
+    return fetch(url, params).then(response => {
+        return response.json();
+    }).then(result => {
+        return result;
+    }).catch(err => {
+        return err.message;
+    })
+}
