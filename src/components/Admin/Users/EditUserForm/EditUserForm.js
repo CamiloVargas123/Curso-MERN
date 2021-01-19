048896887a9de64c038f6dae31007acd8da81efb
Camilo Vargas <camilovargasingm@gmail.com>
2021-01-19 01:43:50 -0500

    mostrando el avatar de user en componet edit

--- src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect, useCallback} from "react";
 import {Avatar, Form, Input, Select, Button, Row, Col} from "antd";
 import {UserOutlined, MailOutlined, LockOutlined} from "@ant-design/icons";
 import {useDropzone} from "react-dropzone";
+
+import {getAvatarApi} from "../../../../api/user";
 import {noAvatar} from "../../../../assets/img";
 
 import "./EditUserForm.scss";
@@ -9,20 +11,35 @@ import "./EditUserForm.scss";
 export default function EditUserForm(props) {
     const {user} = props;
     const [avatar, setAvatar] = useState(null);
-    const [userData, setUserData] = useState({
-        name: user.name,
-        lastname: user.lastname,
-        email: user.email,
-        role: user.role,
-        avatar: user.avatar
-    });
+    const [userData, setUserData] = useState({});
+
     const updateUser = e => {
         console.log(userData);
     }
 
     useEffect(() => {
+        setUserData({
+            name: user.name,
+            lastname: user.lastname,
+            email: user.email,
+            role: user.role,
+            avatar: user.avatar
+        })
+    }, [user]);
+
+    useEffect(() => {
+        if(user.avatar){
+            getAvatarApi(user.avatar).then(response => {
+                setAvatar(response);
+            })
+        }else{
+            setAvatar(null);
+        }
+    }, [user]);
+
+    useEffect(() => {
         if(avatar){
-            setUserData({...userData, avatar})
+            setUserData({...userData, avatar: avatar.file})
         }
     }, [avatar]);
 
@@ -36,6 +53,20 @@ export default function EditUserForm(props) {
 
 function UploadAvatar(props) {
     const {avatar, setAvatar} = props;
+    const [avatarUrl, setAvatarUrl] = useState(null);
+
+    useEffect(() => {
+        if(avatar){
+            if(avatar.preview){
+                setAvatarUrl(avatar.preview)
+            }else{
+                setAvatarUrl(avatar);
+            }
+        }else{
+            setAvatarUrl(null);
+        }
+    }, [avatar]);
+
     const onDrop = useCallback(
         acceptedFiles => {
             const file = acceptedFiles[0];
@@ -53,7 +84,7 @@ function UploadAvatar(props) {
         <div className="upload-avatar" {...getRootProps()}>
             <input {...getInputProps()} />
             {isDragActive ? <Avatar size={150} src={noAvatar} /> : 
-            <Avatar size={150} src={avatar ? avatar.preview : noAvatar} />}
+            <Avatar size={150} src={avatarUrl ? avatarUrl : noAvatar} />}
         </div>
     )
 }
@@ -65,12 +96,12 @@ function EditForm(props) {
             <Row gutter={24}>
                 <Col span={12} >
                     <Form.Item>
-                        <Input prefix={<UserOutlined />} placeholder="Nombre" defaultValue={userData.name} onChange={e => setUserData({...userData, name: e.target.value})} />
+                        <Input prefix={<UserOutlined />} placeholder="Nombre" value={userData.name} onChange={e => setUserData({...userData, name: e.target.value})} />
                     </Form.Item>
                 </Col>
                 <Col span={12} >
                     <Form.Item>
-                        <Input prefix={<UserOutlined />} placeholder="Apellido" defaultValue={userData.lastname} onChange={e => setUserData({...userData, lastname: e.target.value})} />
+                        <Input prefix={<UserOutlined />} placeholder="Apellido" value={userData.lastname} onChange={e => setUserData({...userData, lastname: e.target.value})} />
                     </Form.Item>
                 </Col>
             </Row>
@@ -78,12 +109,12 @@ function EditForm(props) {
             <Row gutter={24}>
                 <Col span={16} >
                     <Form.Item>
-                        <Input prefix={<MailOutlined />} placeholder="Correo" defaultValue={userData.email} onChange={e => setUserData({...userData, email: e.target.value})} />
+                        <Input prefix={<MailOutlined />} placeholder="Correo" value={userData.email} onChange={e => setUserData({...userData, email: e.target.value})} />
                     </Form.Item>
                 </Col>
                 <Col span={8} >
                     <Form.Item>
-                        <Select placeholder="Selecciona el rol" onChange={e => setUserData({...userData, role: e})} defaultValue={userData.role}>
+                        <Select placeholder="Selecciona el rol" onChange={e => setUserData({...userData, role: e})} value={userData.role}>
                             <Select.Option value="admin">Administrador</Select.Option>
                             <Select.Option value="editor">Editor</Select.Option>
                             <Select.Option value="reviewr">Revisor</Select.Option>
