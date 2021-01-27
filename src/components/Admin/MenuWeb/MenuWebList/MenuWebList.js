7b2de80c72dfad80c78c244d9c2af97139b7b3bb
Camilo Vargas <camilovargasingm@gmail.com>
2021-01-27 01:35:25 -0500

    listando menu con accion de Drag and drop from react-drag-sortable

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,15 +1,55 @@
-import Rect from "react";
+import Rect, {useState, useEffect} from "react";
+import {Switch, List, Button, Modal as ModalAndtd, notification} from "antd";
+import {EditOutlined, DeleteOutlined} from "@ant-design/icons";
+import Modal from "../../../Modal";
+import DragSortableList from "react-drag-sortable";
 
 import "./MenuWebList.scss";
 
 export default function MenuWebList(props){
     const {menu, setReloadMenuWeb} = props;
+    const [listItems, setListItems] = useState([]);
+    const [isVisibleModal, setIsVisibleModal] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalContent, setModalContent] = useState(null);
 
-    console.log(menu);
+    console.log(listItems);
+    useEffect(() => {
+        const listItems = [];
+        menu.forEach(item => {
+            listItems.push({
+                content: (<MenuItem item={item} />)
+            })
+        });
+        setListItems(listItems);
+    }, [menu])
+
+    const onSort = (sortedList, dropEvent) => {
+        console.log(sortedList);
+    }
 
     return (
-        <div>
-            {menu.map(item => (<p key={item._id}>{item.title}</p>))}
+        <div className="menu-web-list">
+            <div className="menu-web-list__header">
+                <Button type="primary">Menú</Button>
+            </div>
+            <div className="menu-web-list__items">
+                <DragSortableList items={listItems} onSort={onSort} type="vertical" />
+            </div>
         </div>
+    )
+}
+
+function MenuItem(props){
+    const {item} = props;
+
+    return (
+        <List.Item actions={[
+            <Switch defaultChecked={item.active} />,
+            <Button type="primary" icon={<EditOutlined />} />,
+            <Button type="danger" icon={<DeleteOutlined />} />
+        ]}>
+            <List.Item.Meta title={item.title} description={item.url} />
+        </List.Item>
     )
 }
