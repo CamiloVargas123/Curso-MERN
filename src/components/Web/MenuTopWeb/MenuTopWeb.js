25dea17957f9ce6bbe05fdcb17ed877c57423e17
Camilo Vargas <camilovargasingm@gmail.com>
2021-02-04 04:05:17 -0500

    obteniendo los menu de la base de datos para motrar en home '/'

--- src/components/Web/MenuTopWeb/MenuTopWeb.js
+++ src/components/Web/MenuTopWeb/MenuTopWeb.js
@@ -1,22 +1,47 @@
 import React ,{useState, useEffect} from "react";
 import {Menu} from "antd";
 import { Link } from "react-router-dom";
+import {getMenusApi} from "../../../api/menu";
 import {logo1} from "../../../assets/img/index";
 
 import "./MenuTopWeb.scss";
 
 export default function MenuTop(){
+    const [menuData, setMenuData] = useState([]);
+    
+    useEffect(() => {
+        getMenusApi().then(response => {
+            const arrayMenu = [];
+            
+            response.menusStored.forEach(item => {
+                item.active && arrayMenu.push(item);
+            });
+            setMenuData(arrayMenu);
+        })
+    },[])
+
     return(
         <Menu className="menu-top-web" mode="horizontal">
             <Menu.Item className="menu-top-web__logo">
                 <Link to={"/"}><img src={logo1} alt="Logotipo codesing" /></Link>
             </Menu.Item>
-            <Menu.Item className="menu-top-web__item">
-                <Link to={"/"}>Inicio</Link>                
-            </Menu.Item>
-            <Menu.Item>
-                <Link to={"/contact"}>Contacto</Link>
-            </Menu.Item>
+
+            {menuData.map(item => {
+                const external = item.url.indexOf("http") > -1 ? true : false;
+                if(external){
+                    return(
+                        <Menu.Item key={item._id} className="menu-top-web__item">
+                            <a href={item.url} target="_blank" rel="noreferrer noopener"> {item.title} </a>
+                        </Menu.Item>
+                    )
+                }
+                return(
+                    <Menu.Item key={item._id} className="menu-top-web__item">
+                        <Link to={item.url}> {item.title} </Link>
+                    </Menu.Item>
+                )            
+            })}            
+
             <div>
                 social media
             </div>
