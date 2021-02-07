f401907843757573ff4cf3746379fd08bf7c4d7e
Camilo Vargas <camilovargasingm@gmail.com>
2021-02-07 03:23:58 -0500

    correcion de fallo en el scss

--- src/components/Web/MenuTopWeb/MenuTopWeb.js
+++ src/components/Web/MenuTopWeb/MenuTopWeb.js
@@ -26,23 +26,24 @@ export default function MenuTop(){
             <Menu.Item className="menu-top-web__logo">
                 <Link to={"/"}><img src={logo1} alt="Logotipo codesing" /></Link>
             </Menu.Item>
-            <div className="menu-top-web__menu">
-                {menuData.map(item => {
-                    const external = item.url.indexOf("http") > -1 ? true : false;
-                    if(external){
-                        return(
-                            <Menu.Item key={item._id} className="menu-top-web__item">
-                                <a href={item.url} target="_blank" rel="noreferrer noopener"> {item.title} </a>
-                            </Menu.Item>
-                        )
-                    }
+            
+            <div className="menu-top-web__vacio"></div>
+
+            {menuData.map(item => {
+                const external = item.url.indexOf("http") > -1 ? true : false;
+                if(external){
                     return(
                         <Menu.Item key={item._id} className="menu-top-web__item">
-                            <Link to={item.url}> {item.title} </Link>
+                            <a href={item.url} target="_blank" rel="noreferrer noopener"> {item.title} </a>
                         </Menu.Item>
-                    )            
-                })}
-            </div>      
+                    )
+                }
+                return(
+                    <Menu.Item key={item._id} className="menu-top-web__item">
+                        <Link to={item.url}> {item.title} </Link>
+                    </Menu.Item>
+                )            
+            })}    
 
             <SocialLinks />
         </Menu>
