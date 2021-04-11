c85f25dc9ffb8781868c5e6f1ce975e9ef2a1394
Camilo Vargas <camilovargasingm@gmail.com>
2021-04-11 17:42:53 -0500

    config url for deploy

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,3 @@
-export const BASE_PATH = "http://localhost:3977/api";
+export const BASE_PATH = "https://cursostackmern-server.herokuapp.com/api";
+// export const BASE_PATH = "http://localhost:3977/api";
 export const API_VERSION = "v1";
