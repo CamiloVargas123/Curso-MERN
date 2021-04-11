007df475af2f2682fa548abd820c1daec9de95ff
Camilo Vargas <camilovargasingm@gmail.com>
2021-04-11 17:55:51 -0500

    config url and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Suspense} from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import routes from "./config/Router";
 import {LoadingOutlined} from "@ant-design/icons";
 
